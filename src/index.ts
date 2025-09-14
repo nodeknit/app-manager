14cdf13b3df6deb6e76833e9fc888f70c6b70bde
VuXfi <x@42.pub>
2025-09-14 19:45:50 +0700

    fix: add missing newline at end of file and reorder default export in index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ export * from "./lib/AbstractApp";
 export * from "./lib/CollectionStorage";
 export * from "./lib/decorators/appUtils"
 export * from "./system/handlers/ControllerHandler"
+
+// Default export
+export { AppManager as default } from "./lib/AppManager";
